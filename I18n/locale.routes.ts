227c5414aafc3ee37d40bab85ffd1ac12ee6eefd
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-07-08 13:31:32 +0300

    https://online.sbis.ru/opendoc.html?guid=1328a900-9469-462a-bd10-944ecd2045fa

---
+++ I18n/locale.routes.ts
@@ -0,0 +1,21 @@
+const EXPIRES_COOKIES = 2920;
+
+// @ts-ignore
+module.exports = function() {
+   return {
+      '/locale': function(req, res) {
+         const env = requirejs('Env/Env');
+
+         if (req.query && req.query.lang && env.constants.availableLanguage.hasOwnProperty(req.query.lang)) {
+            env.cookie.set('lang', req.query.lang, {
+               path: '/',
+               expires: EXPIRES_COOKIES
+            });
+         }
+
+         const urlForRedirect = (req.query && req.query.url) ? req.query.url : `${req.protocol}://${req.hostname}`;
+
+         res.redirect(urlForRedirect);
+      }
+   }
+};
