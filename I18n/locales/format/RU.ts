2893834179b96a84f604d1574bd7c2e7275165e4
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-11-15 14:05:24 +0300

    https://online.sbis.ru/opendoc.html?guid=04d271b4-19fe-47de-9640-ae1e8ec1d002

---
+++ I18n/locales/format/RU.ts
@@ -0,0 +1,27 @@
+export default {
+   fullDateDayOfWeekFormat: 'DD MMMMlo\'YY, ddddl',
+   fullDateFormat: 'DD.MM.YY',
+   fullDateFullMonthFormat: 'DD MMMMlo\'YY',
+   fullDateFullMonthFullYearFormat: 'DD MMMMlo YYYY',
+   fullDateFullYearFormat: 'DD.MM.YYYY',
+   fullDateShortMonthFormat: 'DD MMMl\'YY',
+   fullDateShortMonthFullYearFormat: 'DD MMMl YYYY',
+   fullHalfYearFormat: 'YYYYhr \'YY',
+   fullMonthFormat: 'MMMM\'YY',
+   fullQuarterFormat: 'QQQQr \'YY',
+   fullTimeFormat: 'HH:mm:ss',
+   shortDateDayOfWeekFormat: 'DD MMMMlo, ddddl',
+   shortDateFormat: 'DD.MM',
+   shortDateFullMonthFormat: 'DD MMMMlo',
+   shortDateShortMonthFormat: 'DD MMMl',
+   shortHalfYearFormat: 'YYhr \'YY',
+   shortMonthFormat: 'MMM\'YY',
+   shortQuarterFormat: 'QQr \'YY',
+   shortTimeFormat: 'HH:mm',
+   masks: {
+      date: '%d.%m.%y',
+      min: '%H:%M',
+      sec: '%H:%M:%S',
+      msec: '%H:%M:%S.%J'
+   }
+}
---
+++ I18n/locales/language/en.ts
@@ -0,0 +1,60 @@
+export default {
+   /**
+    * Для английской локали
+    * @param pluralNumber число
+    * @param word1 слово для 1
+    * @param word2 слово для нескольких
+    * @returns {String}
+    * @private
+    */
+   plural: (pluralNumber: number, word1: string, word2: string): string => {
+
+      if (pluralNumber > 1 || pluralNumber === 0) {
+         return word2;
+      }
+
+      return word1;
+   },
+   minDays: ['Su', 'Mo', 'To', 'We', 'Th', 'Fr', 'Sa'],
+   shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+   longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+   shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+   longMonths: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+   ],
+   ordinalMonths: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+   ],
+   shortOrdinalMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+   am: 'am',
+   pm: 'pm',
+   minHalfYear: '$digit$s$ hy',
+   longHalfYear: '$digit$s$ half year',
+   minQuarter: '$digit$s$ qt',
+   shortQuarter: '$digit$s$ qtr',
+   longQuarter: '$digit$s$ quarter',
+   quarters: ['I', 'II', 'III', 'IV'],
+   longQuarters: ['I quarter', 'II quarter', 'III quarter', 'IV quarter']
+}
