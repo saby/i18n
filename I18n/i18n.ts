d18979c85396754de44dcf7df94f86157c832fe9
is.kudryavcev <is.kudryavcev@tensor.ru>
2020-07-20 11:20:16 +0300

    https://online.sbis.ru/opendoc.html?guid=2678ba4b-1125-42a3-a51b-b06117480b00

--- I18n/i18n.ts
+++ I18n/i18n.ts
@@ -20,7 +20,4 @@ export {
 
 export {default as Translator} from './_i18n/Translator';
 export {default as Controller} from './_i18n/Controller';
-export {default as Locale} from './_i18n/Locale';
-export {default as Configuration} from './_i18n/Configuration';
-export {default as IConfiguration} from './_i18n/IConfiguration';
 export {default as Loader} from './_i18n/Loader';
