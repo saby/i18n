9f74c737516e139b306efaa283f81613eb16014a
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-08-11 18:02:41 +0300

    new change

---
+++ I18n/_keyboard/detect.ts
@@ -0,0 +1,28 @@
+import Layout from './Layout';
+import type { langCode } from './../interfaces/IAvailableCodes';
+
+/**
+ * Функция определяет язык раскаладки, на котором написан текст.
+ * @param text Текс на основе которого определяется раскалдка.
+ */
+export default function detect(text: string): langCode | undefined {
+    let maxMatch = 0;
+    let result;
+
+    for (const [lang, layout] of Object.entries(Layout.getAll()) as [langCode, string][]) {
+        let match = 0;
+
+        for (const chart of text) {
+            if (layout.includes(chart)) {
+                match += 1;
+            }
+        }
+
+        if (match > maxMatch) {
+            maxMatch = match;
+            result = lang;
+        }
+    }
+
+    return result;
+}
---
+++ I18n/locales/_aspects/TimeInterval/Kazakh.ts
@@ -0,0 +1,41 @@
+import ITimeUnits, { TimeUnitsEntries } from '../../../interfaces/ITimeUnits';
+import plural from '../Plural/Kazakh';
+
+const items = {
+    short: {
+        years: ['ж'],
+        months: ['ай'],
+        weeks: ['апта'],
+        days: ['күн'],
+        hours: ['сағ'],
+        minutes: ['мин'],
+        seconds: ['сек'],
+    },
+    full: {
+        years: ['жыл'],
+        months: ['ай'],
+        weeks: ['апта'],
+        days: ['күн'],
+        hours: ['сағат'],
+        minutes: ['минут'],
+        seconds: ['секунд'],
+    },
+};
+
+/**
+ * Функция форматирования временного интервала в строку для казахского языка.
+ * @public
+ * @author Кудрявцев И.С.
+ */
+export default (interval: ITimeUnits, short: boolean = false) => {
+    const result = [];
+    const units = short ? items.short : items.full;
+
+    for (const [name, value] of Object.entries(interval) as TimeUnitsEntries) {
+        const argv = units[name];
+
+        result.push(`${value} ${plural(value, argv[0])}`);
+    }
+
+    return result.join(' ');
+};
