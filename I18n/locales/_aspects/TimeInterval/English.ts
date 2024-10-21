4552a3c26d40d54326edc3931a41bf6053a44615
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-21 18:02:57 +0300

    new change

---
+++ I18n/locales/_aspects/TimeInterval/English.ts
@@ -0,0 +1,41 @@
+import ITimeUnits, { TimeUnitsEntries } from '../../../interfaces/ITimeUnits';
+import plural from '../Plural/English';
+
+const items = {
+    short: {
+        years: ['yr', 'yr'],
+        months: ['mo', 'mo'],
+        weeks: ['wk', 'wk'],
+        days: ['d', 'd'],
+        hours: ['h', 'h'],
+        minutes: ['min', 'min'],
+        seconds: ['sec', 'sec'],
+    },
+    full: {
+        years: ['year', 'years'],
+        months: ['month', 'months'],
+        weeks: ['week', 'weeks'],
+        days: ['day', 'days'],
+        hours: ['hour', 'hours'],
+        minutes: ['minute', 'minutes'],
+        seconds: ['second', 'seconds'],
+    },
+};
+
+/**
+ * Функция форматирования временного интервала в строку для английского языка.
+ * @public
+ * @author Кудрявцев И.С.
+ */
+export default (interval: ITimeUnits, short: boolean = false) => {
+    const result = [];
+    const units = short ? items.short : items.full;
+
+    for (const [name, value] of Object.entries(interval) as TimeUnitsEntries) {
+        const argv = units[name];
+
+        result.push(`${value} ${plural(value, argv[0], argv[1])}`);
+    }
+
+    return result.join(' ');
+};
