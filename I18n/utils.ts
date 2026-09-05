07b7c79dc583a6efc3b3bea409912d17c4a90da9
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-05 18:04:58 +0300

    new change

---
+++ I18n/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * Библиотека c утилитами для работы с локализацией.
+ * @module
+ * @library
+ * @public
+ * @author Кудрявцев И.С.
+ */
+
+export { getClassifierByRegion, getRegionByClassifier } from './_utils/classifier';
